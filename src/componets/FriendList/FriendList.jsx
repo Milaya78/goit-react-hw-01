c8fe4b398810f3508c919eb6d2c800536f7149c2
Milaya78 <ela_keyt@mail.ru>
2024-09-21 12:21:45 +0200

    project

---
+++ src/componets/FriendList/FriendList.jsx
@@ -0,0 +1,22 @@
+import css from "./FriendList.module.css";
+import FriendListItem from "../FriendListItem/FriendListItem";
+
+export default function FriendList({ friends }) {
+  return (
+    <div className={css.friendListContainer}>
+      <ul className={css.friendList}>
+        {friends.map((item) => {
+          return (
+            <li key={item.id} className={css.friendCard}>
+              <FriendListItem
+                avatar={item.avatar}
+                name={item.name}
+                isOnline={item.isOnline}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
